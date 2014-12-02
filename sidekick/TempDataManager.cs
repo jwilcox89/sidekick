ab34b5c4d8a355c3ccc384e58612c5c2a0ad58b5
Justin Wilcox <jwilcox89@gmail.com>
2014-12-02 11:07:32 -0600

    added temp data manager
    added temp data manager

---
+++ sidekick/TempDataManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace sidekick
+{
+    /// <summary>
+    ///     Designed to help keep TempData values organized.
+    /// </summary>
+    public class TempDataManager
+    {
+        public TempDataDictionary TempData { get; set; }
+
+        public void Add(string key, object value) {
+            Remove(key);
+            TempData.Add(key, value);
+        }
+
+        public void Remove(string key) {
+            if (TempData.ContainsKey(key))
+                TempData.Remove(key);
+        }
+    }
+}
